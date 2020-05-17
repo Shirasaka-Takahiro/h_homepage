834b7b6d3f87953f22d3907b3596e7bb7214cbca
Takahiro <kenshirou913@gmail.com>
2020-05-17 15:26:50 +0900

    finished some systems

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+   @work = Work.all
   end
 
   def show
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,13 +1,46 @@
 class WorksController < ApplicationController
+
   def index
+    @works = Work.all
+    @work = Work.find_by(params[:id])
+    @user = User.find_by(params[:id])
   end
 
   def show
+    @work = Work.find_by(params[:id])
   end
 
   def new
+    @work = Work.new
+    @work = current_user.works.build
   end
 
   def edit
   end
+
+  def create
+    @work = current_user.works.build(work_params)
+
+    if @work.save
+      redirect_to root_url
+      flash[:notice] = "#{@work.title}を投稿しました"
+    else
+      render :new
+    end
+    
+  end
+
+  def destroy
+    @work = Work.find(params[:id])
+    @work.destroy
+    flash[:notice] = "#{@work.title}を削除しました"
+    redirect_to root_url
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :image, :content)
+  end
+
 end
