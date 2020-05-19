87309cabcf8f9dc7e89df963384e11adbcb95c9f
Takahiro <kenshirou913@gmail.com>
2020-05-20 01:19:18 +0900

    finished basic systems of gallery

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -13,5 +13,33 @@ class GalleriesController < ApplicationController
   end
 
   def edit
+    @gallery = Gallery.find(params[:id])
   end
+
+  def create
+
+    @gallery = current_user.galleries.build(gallery_params)
+
+    if @gallery.save
+       redirect_to root_url
+       flash[:notice] = "#{@gallery.title}を投稿しました"
+    else
+       render :new
+    end
+
+  end
+
+  def destroy
+    @gallery = Gallery.find_by(params[:id])
+    @gallery.destroy
+    flash[:notice] = "#{@gallery.title}を削除しました"
+    redirect_to galleries_path   
+  end
+
+  private
+  
+  def gallery_params
+    params.require(:gallery).permit(:title, :content, :image)
+  end
+
 end
